51a1530d07eba6a617f12ef937c0f9b86ba9ec09
Denis <denis.muraska@gmail.com>
2025-06-09 16:33:04 +0100

    fixed cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
-#include <unistd.h>
 
 #include "core/Application.h"
 
 int main() {
-    char cwd[PATH_MAX];
-    if (getcwd(cwd, sizeof(cwd)) != nullptr) {
-        std::cout << "Working directory: " << cwd << std::endl;
-    }
     RendererAPI api = RendererAPI::OpenGL;
     Application app(api);
     app.Run();
