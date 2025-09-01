39459d3ca1e4eb1adebb25c12feb7cfa7e6becd7
metinis <denis.muraska@gmail.com>
2025-09-01 15:34:33 -0400

    CameraComp

--- resources/shaders/glbasic4.1.vert
+++ resources/shaders/glbasic4.1.vert
@@ -1,11 +1,15 @@
 #version 410 core
 
+layout(std140) uniform View {
+  mat4 mat_vp;
+};
+
 layout (location = 0) in vec3 a_pos;
 layout (location = 3) in vec4 a_color;
 
 out vec4 v_color;
 
 void main() {
-  gl_Position = vec4(a_pos, 1.0);
+  gl_Position = mat_vp * vec4(a_pos, 1.0);
   v_color = a_color;
 }
--- src/core/Application.cpp
+++ src/core/Application.cpp
@@ -7,6 +7,8 @@
 #include <ZeusEngineCore/RenderSystem.h>
 #include <ZeusEngineCore/Scene.h>
 
+#include "../../../ZeusEngineCore/src/Systems/Scene/CameraSystem.h"
+
 using namespace ZED;
 Application::Application(ZEN::eRendererAPI api) : m_API(api) {
     m_Window = std::make_unique<ZEN::Window>(1280, 720, "Zeus Editor", m_API);
@@ -27,38 +29,39 @@ Application::Application(ZEN::eRendererAPI api) : m_API(api) {
     ZEN::MeshComp mesh;
 
     mesh.vertices = {
-        // Top-left
-        { { -0.5f,  0.5f, 0.0f },   // Position
-          {  0.0f,  0.0f, 1.0f },   // Normal
-          {  0.0f,  1.0f },         // TexCoords
-          {  1.0f,  1.0f, 1.0f, 1.0f } }, // Color
+        // Front face (z = 0.5)
+        {{-0.5f,  0.5f,  0.5f}, {0,0,1}, {0,1}, {1,0,0,1}}, // 0 top-left-front
+        {{ 0.5f,  0.5f,  0.5f}, {0,0,1}, {1,1}, {0,1,0,1}}, // 1 top-right-front
+        {{ 0.5f, -0.5f,  0.5f}, {0,0,1}, {1,0}, {0,0,1,1}}, // 2 bottom-right-front
+        {{-0.5f, -0.5f,  0.5f}, {0,0,1}, {0,0}, {1,1,0,1}}, // 3 bottom-left-front
 
-        // Top-right
-        { {  0.5f,  0.5f, 0.0f },
-          {  0.0f,  0.0f, 1.0f },
-          {  1.0f,  1.0f },
-          {  1.0f,  1.0f, 1.0f, 1.0f } },
-
-        // Bottom-right
-        { {  0.5f, -0.5f, 0.0f },
-          {  0.0f,  0.0f, 1.0f },
-          {  1.0f,  0.0f },
-          {  1.0f,  1.0f, 1.0f, 1.0f } },
-
-        // Bottom-left
-        { { -0.5f, -0.5f, 0.0f },
-          {  0.0f,  0.0f, 1.0f },
-          {  0.0f,  0.0f },
-          {  1.0f,  1.0f, 1.0f, 1.0f } }
+        // Back face (z = -0.5)
+        {{-0.5f,  0.5f, -0.5f}, {0,0,-1}, {1,1}, {1,0,1,1}}, // 4 top-left-back
+        {{ 0.5f,  0.5f, -0.5f}, {0,0,-1}, {0,1}, {0,1,1,1}}, // 5 top-right-back
+        {{ 0.5f, -0.5f, -0.5f}, {0,0,-1}, {0,0}, {1,1,1,1}}, // 6 bottom-right-back
+        {{-0.5f, -0.5f, -0.5f}, {0,0,-1}, {1,0}, {0,0,0,1}}  // 7 bottom-left-back
     };
 
     mesh.indices = {
-        0, 1, 2,   // First triangle
-        2, 3, 0    // Second triangle
+        // Front
+        0,1,2,  2,3,0,
+        // Right
+        1,5,6,  6,2,1,
+        // Back
+        5,4,7,  7,6,5,
+        // Left
+        4,0,3,  3,7,4,
+        // Top
+        4,5,1,  1,0,4,
+        // Bottom
+        3,2,6,  6,7,3
     };
 
     entt::entity entity = m_Scene->createEntity();
     m_Scene->getRegistry().emplace<ZEN::MeshComp>(entity, mesh);
+
+    entt::entity cameraEntity = m_Scene->createEntity();
+    m_Scene->getRegistry().emplace<ZEN::CameraComp>(cameraEntity);
 }
 Application::~Application() {
      
@@ -77,11 +80,41 @@ void Application::processEvents() {
     
 }
 void Application::onUpdate(float deltaTime) {
+    ZEN::CameraSystem::onUpdate(m_Scene->getRegistry(), m_Window->getWidth(),
+        m_Window->getHeight());
     m_RenderSystem->onUpdate(m_Scene->getRegistry());
 }
 void Application::onUIRender() {
     m_ImGuiLayer->beginFrame();
+
+    // Show demo window (optional)
     ImGui::ShowDemoWindow();
+
+    // Start a new window for camera controls
+    ImGui::Begin("Camera Controls");
+
+    auto view = m_Scene->getRegistry().view<ZEN::CameraComp>();
+    for (auto entity : view) {
+        auto &camera = m_Scene->getRegistry().get<ZEN::CameraComp>(entity);
+        if (camera.isPrimary) {
+            // Create sliders for position
+            ImGui::SliderFloat("Position X", &camera.position.x, -10.0f, 10.0f);
+            ImGui::SliderFloat("Position Y", &camera.position.y, -10.0f, 10.0f);
+            ImGui::SliderFloat("Position Z", &camera.position.z, -10.0f, 10.0f);
+
+            // Optionally sliders for front direction
+            ImGui::SliderFloat("Front X", &camera.front.x, -1.0f, 1.0f);
+            ImGui::SliderFloat("Front Y", &camera.front.y, -1.0f, 1.0f);
+            ImGui::SliderFloat("Front Z", &camera.front.z, -1.0f, 1.0f);
+
+            // Normalize front to avoid stretching
+            camera.front = glm::normalize(camera.front);
+            break; // Only control the first primary camera
+        }
+    }
+
+    ImGui::End();
+
     m_ImGuiLayer->render();
     m_ImGuiLayer->endFrame(nullptr);
 }
